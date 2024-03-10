13f731abe4b673e4f06dc077b66fa093e22b750e
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2024-03-10 22:03:32 +0100

    lkjnkln

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import FriendList from "./components/FriendList/FriendList";
 import FriendListItem from "./components/FriendListItem/FriendListItem";
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
+import userData from "./userData.json";
+import friends from "./friends.json";
+import transaction from "./transactions.json";
+
 const App = () => {
   return (
     <div>
-      <AppProfile />
-      <AppFriendList />
-      <AppFriend />
-      <AppTransactionHistory />
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items ={transactions} />
     </div>
   );
 };
